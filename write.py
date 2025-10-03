a779992b29c68398243f39a62d8e401c3810b0ef
Murilo <murilo.stresser@escola.pr.gov.br>
2025-10-03 10:36:50 -0300

    Delete write.py

--- write.py
+++
@@ -1,4 +0,0 @@
-with open("Meu_Diário.txt","w") as bloco:
-    bloco.write("Hoje, 3 de outubre de 2025, elaborei uma atividade em código sobre arquivo.txt.\n")
-
-print("Arquivo 'Meu_Diário.txt' criado com sucesso!")
