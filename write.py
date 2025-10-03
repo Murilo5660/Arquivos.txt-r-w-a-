126658c482a9ea094107a7fdfb3184c545f20747
Murilo <murilo.stresser@escola.pr.gov.br>
2025-10-03 09:47:17 -0300

    Add files via upload

---
+++ write.py
@@ -0,0 +1,4 @@
+with open("Meu_Diário.txt","w") as bloco:
+    bloco.write("Hoje, 3 de outubre de 2025, elaborei uma atividade em código sobre arquivo.txt.\n")
+
+print("Arquivo 'Meu_Diário.txt' criado com sucesso!")
